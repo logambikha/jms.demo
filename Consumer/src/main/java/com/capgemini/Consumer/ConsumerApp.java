548c424f6a757e350565bf11c65a928593d1569b
Sakthivel <logambikha.sakthivel@capgemini.com>
2018-10-17 08:37:27 -0400

    first

---
+++ Consumer/src/main/java/com/capgemini/Consumer/ConsumerApp.java
@@ -0,0 +1,56 @@
+package com.capgemini.Consumer;
+
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+/**
+ * Hello world!
+ *
+ */
+public class ConsumerApp 
+{
+    public static void main( String[] args )
+    {
+       // System.out.println( "Hello World!" );
+    	 
+        ConnectionFactory connectionFactory;
+        Connection connection = null;
+    
+   try { 
+    InitialContext initialContext = new InitialContext();
+    Queue queue = (Queue) initialContext.lookup("jms/P2PQueue");
+    connectionFactory = (ConnectionFactory) initialContext.lookup("jms/__defaultConnectionFactory");
+    connection = connectionFactory.createConnection();
+    Session session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
+    connection.start();
+    MessageConsumer messageConsumer = session.createConsumer(queue);
+    TextMessage textMessage = (TextMessage) messageConsumer.receive();
+    
+    String body = textMessage.getText();
+    System.out.println(body);
+    
+    
+   } catch(NamingException e) {
+	   e.printStackTrace();
+   } catch (JMSException e) {
+   		e.printStackTrace();
+   }
+   finally {
+	   if(connection != null) try {
+		   connection.close();
+	   } catch (JMSException e) {
+		   e.printStackTrace();
+	   }
+   }
+    	
+    	
+    }
+}
---
+++ Producer/src/main/java/com/capgemini/Producer/ProducerApp.java
@@ -0,0 +1,54 @@
+package com.capgemini.Producer;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class ProducerApp 
+{
+    public static void main( String[] args )
+    {
+    	
+       // System.out.println( "Hello World!" );
+        
+        ConnectionFactory connectionFactory;
+        Connection connection = null;
+    
+   try { 
+    InitialContext initialContext = new InitialContext();
+    Queue queue = (Queue) initialContext.lookup("jms/P2PQueue");
+    connectionFactory = (ConnectionFactory) initialContext.lookup("jms/__defaultConnectionFactory");
+    
+    connection = connectionFactory.createConnection();
+    Session session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
+   
+    MessageProducer messageProducer = session.createProducer(queue);
+    TextMessage textMessage = session.createTextMessage("logi");
+    messageProducer.send(textMessage);
+    System.out.println("Message Produced");
+   } catch(NamingException e) {
+	   e.printStackTrace();
+   } catch (JMSException e) {
+   		e.printStackTrace();
+   }
+   finally {
+	   if(connection != null) try {
+		   connection.close();
+	   } catch (JMSException e) {
+		   e.printStackTrace();
+	   }
+   }
+  }
+}
